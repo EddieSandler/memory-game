bb5c878d22343c465938f86150ed3909435cf1ff
esand68 <ed.sandler@gmail.com>
2023-06-29 16:50:46 -0400

    added functionality to prevent double clicking

--- memory-game.js
+++ memory-game.js
@@ -12,147 +12,77 @@ const colors = shuffle(COLORS);
 
 createCards(colors);
 
+let selectedCards = []; // Global variable to store clicked cards
 
 /** Shuffle array items in-place and return shuffled array. */
-
 function shuffle(items) {
-  // This algorithm does a "perfect shuffle", where there won't be any
-  // statistical bias in the shuffle (many naive attempts to shuffle end up not
-  // be a fair shuffle). This is called the Fisher-Yates shuffle algorithm; if
-  // you're interested, you can learn about it, but it's not important.
-
   for (let i = items.length - 1; i > 0; i--) {
-    // generate a random index between 0 and i
     let j = Math.floor(Math.random() * i);
-    // swap item at i <-> item at j
     [items[i], items[j]] = [items[j], items[i]];
   }
-
   return items;
 }
 
-/** Create card for every color in colors (each will appear twice)
- *
- * Each div DOM element will have:
- * - a class with the value of the color
- * - a click event listener for each card to handleCardClick
- */
-
+/** Create card for every color in colors (each will appear twice) */
 function createCards(colors) {
   const gameBoard = document.getElementById("game");
 
   for (let color of colors) {
-    // missing code here ...
+    const card = document.createElement('div');
+    card.id = color;
+    card.classList.add(color);
+    card.clicked = false; // Property to track if the card is clicked
+    gameBoard.appendChild(card);
+  }
 
-    const card=document.createElement('div')
-    card.id=color
-    card.classList.add(color)
-    gameBoard.appendChild(card)
-
-    }
-
-    gameBoard.addEventListener("click",function(e) {
-
-      // console.log(e.target.classList)
-
-
-
-
-        handleCardClick(e)
-  })
-
+  gameBoard.addEventListener("click", handleCardClick);
 }
-
 
 /** Flip a card face-up. */
-
 function flipCard(card) {
-  // ... you need to write this ...
-  card.classList.toggle('flipped')
-  card.style.backgroundColor=card.id
-
- let flippedCards=document.querySelectorAll("div.flipped")
-
-let score =0
-
- let firstCard=flippedCards[0]
-//  console.log(firstCard.id)
-
-
- if(flippedCards.length > 1) {
- let secondCard=flippedCards[1] //
-  // console.log(secondCard.id)
-  if (firstCard.id === secondCard.id){
-    console.log("MATCH")
-    firstCard.classList.toggle('matched')
-    secondCard.classList.toggle('matched')
-    // console.log(secondCard.classList)
-
-
-    let matched=document.querySelectorAll("div.matched")
-    console.log(matched)
-    for(let element of matched){
-      element.remove()
-    }
-
-
-
-    //can i remove the event listener for these cards
-    // document.removeEventListener('click',function(e){
-      // handleCardClick(e)
-    // })
-
-
-
-
-  } else {
-    for(card of flippedCards) {
-      unFlipCard(card)
-    }
-  }
+  card.classList.toggle('flipped');
+  card.style.backgroundColor = card.id;
 }
- }
-
 
 /** Flip a card face-down. */
-
 function unFlipCard(card) {
-  // ... you need to write this ...
-
-  card.classList.toggle('flipped')
-  card.classList.toggle('clicked')
+  card.classList.toggle('flipped');
+  card.classList.toggle('clicked');
   setTimeout(() => {
-    card.style.backgroundColor='';
+    card.style.backgroundColor = '';
   }, "1000");
-
 }
 
 /** Handle clicking on a card: this could be first-card or second-card. */
-
 function handleCardClick(evt) {
+  const clickedCard = evt.target;
 
-// //toggle card attribute to'clicked'
-let clickedCard=evt.target
+  if (selectedCards.includes(clickedCard)) {
+    return; // Return early if the card is already selected
+  }
 
-clickedCard.classList.toggle('clicked')
+  clickedCard.clicked = true; // Mark the card as clicked
+  selectedCards.push(clickedCard);
 
-//set first and second cards to the selected cards
-let selectedCards=document.querySelectorAll("div.clicked")
+  flipCard(clickedCard);
 
+  if (selectedCards.length === 2) {
+    const firstCard = selectedCards[0];
+    const secondCard = selectedCards[1];
 
+    if (firstCard.id === secondCard.id) {
+      alert("MATCH");
+      firstCard.classList.toggle('matched');
+      secondCard.classList.toggle('matched');
 
+      let matched = document.querySelectorAll("div.matched");
+      return matched
+    } else {
+      for (let card of selectedCards) {
+        unFlipCard(card);
+      }
+    }
 
-if(selectedCards.length<=2){
-  flipCard(clickedCard)
+    selectedCards = []; // Reset the selected cards array
+  }
 }
-
-}
-
-
-
-
-
-
-
-
-
